76163caa4a077a68ce369f370143d09957caf1c0
unknown <m.sharipov2@student.inha.uz>
2019-03-22 21:58:38 +0500

    content

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,29 +1,28 @@
+<!-----------------------php------------------------------>
 <?php
-
 	$pattern="";
 	$text="";
-	$replaceText="";
+	$repText="";
 	$replacedText="";
-
 	$match="Not checked yet.";
 
-if ($_SERVER["REQUEST_METHOD"]=="POST") {
+if ($_SERVER["REQUEST_METHOD"]=="POST") 
+{
 	$pattern=$_POST["pattern"];
+    
 	$text=$_POST["text"];
+    
 	$replaceText=$_POST["replaceText"];
-
-	$replacedText=preg_replace($pattern, $replaceText, $text);
-
-	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
-}
-
+    
+	$replacedText=preg_replace($pattern, $repText, $text);
+        if(preg_match($pattern, $text)) {
+            $match="Match!";
+        } else {
+            $match="Does not match!";
+        }
+    }
 ?>
-
-
+<!----------------------------main part---------------------------------->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
 
 			<dt>Replace Text</dt>
-			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
+			<dd><input type="text" name="replaceText" value="<?= $repText ?>"></dd>
 
 			<dt>Output Text</dt>
 			<dd><?=	$match ?></dd>
